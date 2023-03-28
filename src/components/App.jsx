6bdca48dacbea67f95a1ac87737691d2eeb84ced
Anastasiia2827 <nasty.bond2016@gmail.com>
2023-03-28 22:15:49 +0300

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { GlobalStyle } from './GlobalStyle';
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import { useState } from 'react';
 
 import { FeedbackButton } from './FeedbackButton';
 import { Section } from './Section';
@@ -7,40 +8,40 @@ import { Statistics } from './Statistics';
 
 import { Container } from './App.styled';
 
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
-
-  addFeedback = e => {
+  function addFeedback(e) {
     const { id } = e.target;
-    this.setState(prevState => {
-      return { [id]: prevState[id] + 1 };
-    });
-  };
 
-  totalFeedback() {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
+    switch (id) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   }
 
-  positiveFeedbackPercentage() {
-  const total = this.totalFeedback();
-  return Math.round((this.state.good * 100) / total);
-}
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.totalFeedback();
-    return (
+  const total = good + bad + neutral;
+  const positiveFeedbackPercentage = Math.round((good * 100) / total);
+
+      return (
       <Container>
-        <GlobalStyle />
+          <GlobalStyle />
+          
         <Section title="Please leave feedback">
   <FeedbackButton
     options={{ good: 'Good', neutral: 'Neutral', bad: 'Bad' }}
-    addFeedback={this.addFeedback}
+    addFeedback={addFeedback}
   ></FeedbackButton>
         </Section>
 
@@ -52,12 +53,69 @@ export class App extends Component {
       good={good}
       neutral={neutral}
       bad={bad}
-      total={this.totalFeedback()}
-      positivePercentage={this.positiveFeedbackPercentage()}
+      total={total}
+      positivePercentage={positiveFeedbackPercentage}
     ></Statistics>
   )}
 </Section>
         </Container>
     );
-  }
-};
+}
+  
+export {App}
+
+
+
+// export class App extends Component {
+  // state = {
+    // good: 0,
+    // neutral: 0,
+    // bad: 0
+  // };
+
+  // addFeedback = e => {
+    // const { id } = e.target;
+    // this.setState(prevState => {
+      // return { [id]: prevState[id] + 1 };
+    // });
+  // };
+
+  // totalFeedback() {
+    // const { good, neutral, bad } = this.state;
+    // return good + neutral + bad;
+  // }
+
+  // positiveFeedbackPercentage() {
+  // const total = this.totalFeedback();
+  // return Math.round((this.state.good * 100) / total);
+// }
+  // render() {
+    // const { good, neutral, bad } = this.state;
+    // const total = this.totalFeedback();
+//     return (
+//       <Container>
+//         <GlobalStyle />
+//         <Section title="Please leave feedback">
+//   <FeedbackButton
+//     options={{ good: 'Good', neutral: 'Neutral', bad: 'Bad' }}
+//     addFeedback={this.addFeedback}
+//   ></FeedbackButton>
+//         </Section>
+
+//         <Section title="Statistics">
+//   {!total ? (
+//    <p>There is no feedback</p>
+//   ) : (
+//     <Statistics
+//       good={good}
+//       neutral={neutral}
+//       bad={bad}
+//       total={this.totalFeedback()}
+//       positivePercentage={this.positiveFeedbackPercentage()}
+//     ></Statistics>
+//   )}
+// </Section>
+//         </Container>
+//     );
+//   }
+// };
